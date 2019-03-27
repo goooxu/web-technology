69ebfc1c5737afd2cfd3187a7c8a158cecd64fe9
Gems <22703054+goooxu@users.noreply.github.com>
2019-03-27 17:31:23 +0800

    Add cross web communication support

--- closedcircuit.jsx
+++ closedcircuit.jsx
@@ -1,4 +1,11 @@
 const CandidatePieceTypes = [10, 5, 12, 6, 3, 9];
+const ApiBaseUrl = 'https://closedcircuit.azurewebsites.net/api';
+
+function uniqueIdentity() {
+    const array = new Uint8Array(8);
+    window.crypto.getRandomValues(array);
+    return window.btoa(array);
+}
 
 class PieceView extends React.Component {
     constructor(props) {
@@ -159,12 +166,60 @@ class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            identity: uniqueIdentity(),
+            remainingPieces: 48,
+            dropIndexesInTurn: [],
             movingPieceType: 0,
             chessPieces: Array(256).fill().map(() => new Piece())
         };
         this.moveStart = this.moveStart.bind(this);
         this.moveEnd = this.moveEnd.bind(this);
         this.checkAvailable = this.checkAvailable.bind(this);
+        this.onReceiveMessage = this.onReceiveMessage.bind(this);
+        this.handleEndTurnClick = this.handleEndTurnClick.bind(this);
+    }
+
+    componentDidMount() {
+        const connection = new signalR.HubConnectionBuilder()
+            .withUrl(ApiBaseUrl)
+            .configureLogging(signalR.LogLevel.Information)
+            .build();
+        connection.on('message', this.onReceiveMessage);
+        connection.onclose(() => console.log('disconnected'));
+
+        console.log('connecting...');
+        connection.start().then(() => console.log('connected')).catch(console.error);
+    }
+
+    onReceiveMessage(message) {
+        console.log('Received message: ', message);
+        if (message.identity !== this.state.identity) {
+            if (message.action === 'move') {
+                this.onReceiveMoveAction(message.index, message.pieceType);
+            }
+        }
+    }
+
+    onReceiveMoveAction(index, pieceType) {
+        this.setState(state => {
+            this.move(state, index, pieceType);
+            return state;
+        });
+    }
+
+    sendMoveAction(index, pieceType) {
+        fetch(`${ApiBaseUrl}/message`, { method: 'POST', body: JSON.stringify({ identity: this.state.identity, action: 'move', index, pieceType }) });
+    }
+
+    handleEndTurnClick() {
+        if (this.state.dropIndexesInTurn.length !== 0) {
+            this.setState(
+                state => {
+                    state.dropIndexesInTurn.length = 0;
+                    return state;
+                }
+            );
+        }
     }
 
     moveStart(pieceType) {
@@ -177,13 +232,24 @@ class Game extends React.Component {
                 state => {
                     state.movingPieceType = 0;
                     if (index !== -1) {
-                        state.chessPieces[index].occupy(pieceType);
-                        this.increaseSurroundingPieceLinks(state.chessPieces, index, pieceType);
+                        this.move(this.state, index, pieceType);
+                        if (state.dropIndexesInTurn.length < 2) {
+                            state.dropIndexesInTurn.push(index);
+                        } else {
+                            state.dropIndexesInTurn.length = 0;
+                        }
+                        this.sendMoveAction(index, pieceType);
                     }
                     return state;
                 }
             );
         }
+    }
+
+    move(state, index, pieceType) {
+        state.remainingPieces -= 1;
+        state.chessPieces[index].occupy(pieceType);
+        this.increaseSurroundingPieceLinks(state.chessPieces, index, pieceType);
     }
 
     getNeighboringIndex(index, direction) {
@@ -227,8 +293,9 @@ class Game extends React.Component {
         }
     }
 
-    travelForLoop(index, pieceType) {
+    checkForLoop(index) {
         const startIndex = index;
+        let pieceType = this.state.movingPieceType;
         let step = 0;
         let sourceDirection = 0;
 
@@ -270,9 +337,21 @@ class Game extends React.Component {
         return [true, step];
     }
 
-    checkAvailable(index, movingPieceType) {
+    checkForNeighbor(index, originIndex, distance) {
+        const rowOffset = Math.abs(Math.floor(index / 16) - Math.floor(originIndex / 16));
+        const columnOffset = Math.abs(index % 16 - originIndex % 16);
+
+        return ((rowOffset <= distance) && columnOffset === 0) ||
+            ((columnOffset <= distance) && rowOffset === 0);
+    }
+
+    checkAvailable(index) {
         if (this.state.chessPieces[index].occupied) {
             return 1;
+        }
+
+        if (!this.state.dropIndexesInTurn.every(dropIndex => this.checkForNeighbor(index, dropIndex, this.state.dropIndexesInTurn.length))) {
+            return 0;
         }
 
         for (let direction = 0; direction < 4; direction++) {
@@ -283,23 +362,23 @@ class Game extends React.Component {
             if (oppositeIndex !== -1) {
                 const oppositePiece = this.state.chessPieces[oppositeIndex];
                 if (oppositePiece.occupied) {
-                    if ((movingPieceType & mask) === 0 && (oppositePiece.type & oppositeMask) !== 0) {
+                    if ((this.state.movingPieceType & mask) === 0 && (oppositePiece.type & oppositeMask) !== 0) {
                         return 0;
                     }
-                    if ((movingPieceType & mask) !== 0 && (oppositePiece.type & oppositeMask) === 0) {
+                    if ((this.state.movingPieceType & mask) !== 0 && (oppositePiece.type & oppositeMask) === 0) {
                         return 0;
                     }
                 } else if (oppositePiece.blank) {
-                    if ((movingPieceType & mask) !== 0 && oppositePiece.links >= 2) {
+                    if ((this.state.movingPieceType & mask) !== 0 && oppositePiece.links >= 2) {
                         return 0;
                     }
                 }
-            } else if ((movingPieceType & mask) !== 0) {
+            } else if ((this.state.movingPieceType & mask) !== 0) {
                 return 0;
             }
         }
 
-        const [success, length] = this.travelForLoop(index, movingPieceType);
+        const [success, length] = this.checkForLoop(index);
         if (success) {
             console.log('Found loop, the length is', length);
             return 0;
@@ -310,13 +389,13 @@ class Game extends React.Component {
 
     render() {
         return <React.Fragment>
-            <h1>Closed Circui</h1>
+            <h1 className="center">Closed Circuit</h1>
             <div className="board">
                 {this.state.chessPieces.map((piece, i) => {
                     if (piece.occupied) {
                         return <ChessGrid key={i} index={i} state={1} pieceType={piece.type} />
                     } else if (this.state.movingPieceType !== 0) {
-                        return <ChessGrid key={i} index={i} state={this.checkAvailable(i, this.state.movingPieceType)} moveEnd={this.moveEnd} />
+                        return <ChessGrid key={i} index={i} state={this.checkAvailable(i)} moveEnd={this.moveEnd} />
                     } else {
                         return <ChessGrid key={i} index={i} state={0} />
                     }
@@ -324,11 +403,15 @@ class Game extends React.Component {
             </div>
             <hr></hr>
             <div className="candidates">
+                <span>Remaining <b>{this.state.remainingPieces}</b> pieces in total</span>
                 {CandidatePieceTypes.map(i =>
                     <CandidateGrid key={i} pieceType={i} moveStart={this.moveStart} moveEnd={this.moveEnd} />
                 )}
+                <span>Remaining <b>{3 - this.state.dropIndexesInTurn.length}</b> pieces in this turn</span>
+                <button onClick={this.handleEndTurnClick}>End turn</button>
             </div>
-
+            <hr></hr>
+            <p className="center">Powered by GitHub Pages, React, Azure Functions, SignalR Service</p>
         </React.Fragment>;
     }
 }
